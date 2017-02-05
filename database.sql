b2ad461fcc11d599ad1f4deb8c3b9e8d8e4f0226
KifKick <kifkick@gmail.com>
2017-02-05 05:39:08 +0100

    Update database.sql

--- database.sql
+++ database.sql
@@ -10,10 +10,31 @@ Target Server Type    : MYSQL
 Target Server Version : 50624
 File Encoding         : 65001
 
-Date: 2017-01-21 15:52:54
+Date: 2017-02-05 05:38:54
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for actionlog
+-- ----------------------------
+DROP TABLE IF EXISTS `actionlog`;
+CREATE TABLE `actionlog` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `playerUID` varchar(50) NOT NULL,
+  `playerName` varchar(64) NOT NULL,
+  `playerCash` int(13) NOT NULL,
+  `playerBank` int(13) NOT NULL,
+  `type` varchar(20) NOT NULL,
+  `text` varchar(255) NOT NULL,
+  `unitUID` varchar(50) NOT NULL,
+  `unitName` varchar(64) NOT NULL,
+  `unitCash` int(13) NOT NULL,
+  `unitBank` int(13) NOT NULL,
+  `amount` varchar(100) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for garage
@@ -21,7 +42,7 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `garage`;
 CREATE TABLE `garage` (
   `license` varchar(32) NOT NULL,
-  `class` varchar(32) NOT NULL,
+  `class` varchar(64) NOT NULL,
   `color` varchar(32) NOT NULL,
   `finish` varchar(32) NOT NULL,
   `rims` varchar(32) NOT NULL,
@@ -47,7 +68,7 @@ CREATE TABLE `logs` (
   `pos` varchar(50) NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=5040 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=52151 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Table structure for mafiaatm
@@ -64,64 +85,64 @@ CREATE TABLE `mafiaatm` (
 -- ----------------------------
 DROP TABLE IF EXISTS `mail`;
 CREATE TABLE `mail` (
-  `message` varchar(32) NOT NULL,
-  `title` varchar(32) NOT NULL,
-  `sender` varchar(32) NOT NULL,
-  `receiver` varchar(32) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `message` varchar(500) CHARACTER SET latin1 NOT NULL,
+  `title` varchar(32) CHARACTER SET latin1 NOT NULL,
+  `sender` varchar(32) CHARACTER SET latin1 NOT NULL,
+  `receiver` varchar(32) CHARACTER SET latin1 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 -- Table structure for messages
 -- ----------------------------
 DROP TABLE IF EXISTS `messages`;
 CREATE TABLE `messages` (
-  `message` varchar(256) NOT NULL,
-  `title` varchar(64) NOT NULL,
-  `sender` varchar(64) NOT NULL,
-  `receiver` varchar(64) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `message` varchar(256) CHARACTER SET latin1 NOT NULL,
+  `title` varchar(64) CHARACTER SET latin1 NOT NULL,
+  `sender` varchar(64) CHARACTER SET latin1 NOT NULL,
+  `receiver` varchar(64) CHARACTER SET latin1 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 -- Table structure for users
 -- ----------------------------
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-  `uid` varchar(50) DEFAULT NULL,
-  `name` varchar(50) DEFAULT NULL,
-  `items` varchar(1500) NOT NULL,
+  `uid` varchar(50) CHARACTER SET latin1 DEFAULT NULL,
+  `name` varchar(50) CHARACTER SET latin1 DEFAULT NULL,
+  `items` varchar(1500) CHARACTER SET latin1 NOT NULL,
   `cash` int(13) NOT NULL,
   `bank` int(13) NOT NULL,
   `cop` int(13) NOT NULL,
   `ems` int(13) NOT NULL,
-  `position` varchar(50) NOT NULL,
+  `position` varchar(50) CHARACTER SET latin1 NOT NULL,
   `bankaccount` int(11) NOT NULL AUTO_INCREMENT,
-  `phoneBackground` varchar(50) NOT NULL,
-  `messages` varchar(5000) NOT NULL,
-  `statuses` text NOT NULL,
-  `houselevel` enum('1','2','3') NOT NULL DEFAULT '1',
-  `housecontent` varchar(1500) NOT NULL,
-  `shopcontent` varchar(1500) NOT NULL,
-  `shopname` varchar(32) NOT NULL,
-  `mafia` enum('0','1','2','3','4','5','6','7','8','9','10') NOT NULL DEFAULT '0',
-  `fire` enum('0','1','2','3','4','5','6','7','8','9','10') NOT NULL DEFAULT '0',
-  `legal` enum('0','1','2','3','4','5','6','7','8','9','10') NOT NULL DEFAULT '0',
+  `phoneBackground` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `messages` varchar(5000) CHARACTER SET latin1 NOT NULL,
+  `statuses` text CHARACTER SET latin1 NOT NULL,
+  `houselevel` enum('1','2','3') CHARACTER SET latin1 NOT NULL DEFAULT '1',
+  `housecontent` varchar(1500) CHARACTER SET latin1 NOT NULL,
+  `shopcontent` varchar(1500) CHARACTER SET latin1 NOT NULL,
+  `shopname` varchar(32) CHARACTER SET latin1 NOT NULL,
+  `mafia` enum('0','1','2','3','4','5','6','7','8','9','10') CHARACTER SET latin1 NOT NULL DEFAULT '0',
+  `fire` enum('0','1','2','3','4','5','6','7','8','9','10') CHARACTER SET latin1 NOT NULL DEFAULT '0',
+  `legal` enum('0','1','2','3','4','5','6','7','8','9','10') CHARACTER SET latin1 NOT NULL DEFAULT '0',
   `prison` tinyint(1) NOT NULL DEFAULT '0',
-  `prisonreason` varchar(32) NOT NULL,
-  `mayor` enum('0','1') NOT NULL DEFAULT '0',
-  `doughnuts` enum('0','1','2','3','4','5') NOT NULL DEFAULT '0',
-  `connected` enum('0','1') NOT NULL DEFAULT '0',
+  `prisonreason` varchar(32) CHARACTER SET latin1 NOT NULL,
+  `mayor` enum('0','1') CHARACTER SET latin1 NOT NULL DEFAULT '0',
+  `doughnuts` enum('0','1','2','3','4','5') CHARACTER SET latin1 NOT NULL DEFAULT '0',
+  `connected` enum('0','1') CHARACTER SET latin1 NOT NULL DEFAULT '0',
   PRIMARY KEY (`bankaccount`),
   UNIQUE KEY `uid` (`uid`)
-) ENGINE=InnoDB AUTO_INCREMENT=398 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=620 DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 -- Table structure for wanted
 -- ----------------------------
 DROP TABLE IF EXISTS `wanted`;
 CREATE TABLE `wanted` (
-  `suspectID` varchar(32) NOT NULL,
-  `officerGUID` varchar(32) NOT NULL,
-  `charges` text NOT NULL,
+  `suspectID` varchar(32) CHARACTER SET utf8 NOT NULL,
+  `officerGUID` varchar(32) CHARACTER SET utf8 NOT NULL,
+  `charges` text CHARACTER SET utf8 NOT NULL,
   `status` tinyint(1) NOT NULL DEFAULT '0',
-  `evidence` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `evidence` text CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
