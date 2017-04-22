d4264ee438e185a9887ad35dce8eeb9fd95533d6
Kruk939 <kajetan@smsnet.pl>
2017-04-22 15:03:03 +0200

    Dialogi
    Tworznie firmy, rozpoczynanie pracy, zarządzanie firmą i bankomat
    firmowy

--- SERWER #1/SL_NoPixel_0.0.0.Jackson_County/dialogs/kruk_company.hpp
+++ SERWER #1/SL_NoPixel_0.0.0.Jackson_County/dialogs/kruk_company.hpp
@@ -141,7 +141,76 @@ class kruk_company_create {
 	enableSimulation = 1;
 	onLoad = "";
 	class controls {
-		
+		class edit_shortucut: Client_RscEdit {
+			idc = 1001;
+			text = "";
+			autocomplete = "";
+			x = 20.5 * GUI_GRID_W + GUI_GRID_X;
+			y = 4 * GUI_GRID_H + GUI_GRID_Y;
+			w = 10.5 * GUI_GRID_W;
+			h = 2 * GUI_GRID_H;
+			colorBackground[] = {0,0,0,0.5};
+		};
+		class edit_full_name: Client_RscEdit {
+			idc = 1002;
+			x = 20.5 * GUI_GRID_W + GUI_GRID_X;
+			y = 7 * GUI_GRID_H + GUI_GRID_Y;
+			w = 10.5 * GUI_GRID_W;
+			h = 2 * GUI_GRID_H;
+			colorBackground[] = {0,0,0,0.5};
+		};
+		class button_close: client_RscButtonMenu {
+			idc = 1201;
+			text = "Zamknij"; //--- ToDo: Localize;
+			x = 22 * GUI_GRID_W + GUI_GRID_X;
+			y = 23 * GUI_GRID_H + GUI_GRID_Y;
+			w = 9.5 * GUI_GRID_W;
+			h = 2 * GUI_GRID_H;
+		};
+		class button_create: client_RscButtonMenu {
+			idc = 1202;
+			text = "Utwórz"; //--- ToDo: Localize;
+			x = 10 * GUI_GRID_W + GUI_GRID_X;
+			y = 23 * GUI_GRID_H + GUI_GRID_Y;
+			w = 12 * GUI_GRID_W;
+			h = 2 * GUI_GRID_H;
+		};
+		class text_price: RscText {
+			idc = 1301;
+			text = "Cena: $50000"; //--- ToDo: Localize;
+			x = 10 * GUI_GRID_W + GUI_GRID_X;
+			y = 20.5 * GUI_GRID_H + GUI_GRID_Y;
+			w = 21.5 * GUI_GRID_W;
+			h = 2 * GUI_GRID_H;
+			colorBackground[] = {0,0,0,0.5};
+		};
+		class text_shortcut: RscText {
+			idc = -1;
+			text = "Skrót"; //--- ToDo: Localize;
+			x = 10 * GUI_GRID_W + GUI_GRID_X;
+			y = 4 * GUI_GRID_H + GUI_GRID_Y;
+			w = 10.5 * GUI_GRID_W;
+			h = 2 * GUI_GRID_H;
+			colorBackground[] = {0,0,0,0.5};
+		};
+		class text_full_name: RscText {
+			idc = -1;
+			text = "Nazwa firmy"; //--- ToDo: Localize;
+			x = 10 * GUI_GRID_W + GUI_GRID_X;
+			y = 7 * GUI_GRID_H + GUI_GRID_Y;
+			w = 10.5 * GUI_GRID_W;
+			h = 2 * GUI_GRID_H;
+			colorBackground[] = {0,0,0,0.5};
+		};
+		class text_title: RscText {
+			idc = -1;
+			text = "Zakładanie firmy"; //--- ToDo: Localize;
+			x = 0 * GUI_GRID_W + GUI_GRID_X;
+			y = 0 * GUI_GRID_H + GUI_GRID_Y;
+			w = 40 * GUI_GRID_W;
+			h = 1 * GUI_GRID_H;
+			colorBackground[] = {0,0,0,0.5};
+		};
 	};
 };
 class kruk_company_preview {
@@ -151,7 +220,183 @@ class kruk_company_preview {
 	enableSimulation = 1;
 	onLoad = "";
 	class controls {
-		
+		class edit_vehicle_level: Client_RscEdit {
+			idc = 1001;
+			text = "";
+			autocomplete = "";
+			x = 0 * GUI_GRID_W + GUI_GRID_X;
+			y = 10 * GUI_GRID_H + GUI_GRID_Y;
+			w = 6.5 * GUI_GRID_W;
+			h = 1.5 * GUI_GRID_H;
+			colorBackground[] = {0,0,0,0.5};
+		};
+		class edit_employee_level: Client_RscEdit {
+			idc = 1002;
+			text = "";
+			autocomplete = "";
+			x = 20.5 * GUI_GRID_W + GUI_GRID_X;
+			y = 10 * GUI_GRID_H + GUI_GRID_Y;
+			w = 6.5 * GUI_GRID_W;
+			h = 1.5 * GUI_GRID_H;
+			colorBackground[] = {0,0,0,0.5};
+		};
+		class listbox_logs: client_RscListBox {
+			idc = 1101;
+			x = 0 * GUI_GRID_W + GUI_GRID_X;
+			y = 13 * GUI_GRID_H + GUI_GRID_Y;
+			w = 19.5 * GUI_GRID_W;
+			h = 7.5 * GUI_GRID_H;
+			colorBackground[] = {0,0,0,0.5};
+		};
+		class listbox_applicants: client_RscListBox {
+			idc = 1102;
+			x = 20.5 * GUI_GRID_W + GUI_GRID_X;
+			y = 13 * GUI_GRID_H + GUI_GRID_Y;
+			w = 19.5 * GUI_GRID_W;
+			h = 7.5 * GUI_GRID_H;
+			colorBackground[] = {0,0,0,0.5};
+		};
+		class listbox_employees: client_RscListBox {
+			idc = 1103;
+			x = 20.5 * GUI_GRID_W + GUI_GRID_X;
+			y = 2.5 * GUI_GRID_H + GUI_GRID_Y;
+			w = 19.5 * GUI_GRID_W;
+			h = 7.5 * GUI_GRID_H;
+			colorBackground[] = {0,0,0,0.5};
+		};
+		class list_box_vehicles: client_RscListBox {
+			idc = 1104;
+			x = 0 * GUI_GRID_W + GUI_GRID_X;
+			y = 2.5 * GUI_GRID_H + GUI_GRID_Y;
+			w = 19.5 * GUI_GRID_W;
+			h = 7.5 * GUI_GRID_H;
+			colorBackground[] = {0,0,0,0.5};
+		};
+		class button_vehicle_check: client_RscButtonMenu {
+			idc = 1201;
+			text = "Sprawdz"; //--- ToDo: Localize;
+			x = 13 * GUI_GRID_W + GUI_GRID_X;
+			y = 10 * GUI_GRID_H + GUI_GRID_Y;
+			w = 6.5 * GUI_GRID_W;
+			h = 1.5 * GUI_GRID_H;
+		};
+		class button_vehicle_change_level: client_RscButtonMenu {
+			idc = 1202;
+			text = "Zmien poziom"; //--- ToDo: Localize;
+			x = 6.5 * GUI_GRID_W + GUI_GRID_X;
+			y = 10 * GUI_GRID_H + GUI_GRID_Y;
+			w = 6.5 * GUI_GRID_W;
+			h = 1.5 * GUI_GRID_H;
+		};
+		class button_employee_change_level: client_RscButtonMenu {
+			idc = 1203;
+			text = "Zmien poziom"; //--- ToDo: Localize;
+			x = 27 * GUI_GRID_W + GUI_GRID_X;
+			y = 10 * GUI_GRID_H + GUI_GRID_Y;
+			w = 6.5 * GUI_GRID_W;
+			h = 1.5 * GUI_GRID_H;
+		};
+		class button_employee_check: client_RscButtonMenu {
+			idc = 1204;
+			text = "Sprawdz"; //--- ToDo: Localize;
+			x = 33.5 * GUI_GRID_W + GUI_GRID_X;
+			y = 10 * GUI_GRID_H + GUI_GRID_Y;
+			w = 6.5 * GUI_GRID_W;
+			h = 1.5 * GUI_GRID_H;
+		};
+		class button_logs_last_operations: client_RscButtonMenu {
+			idc = 1205;
+			text = "Ostatnie operacje"; //--- ToDo: Localize;
+			x = 13 * GUI_GRID_W + GUI_GRID_X;
+			y = 20.5 * GUI_GRID_H + GUI_GRID_Y;
+			w = 6.5 * GUI_GRID_W;
+			h = 1.5 * GUI_GRID_H;
+		};
+		class button_logs_vehicles: client_RscButtonMenu {
+			idc = 1206;
+			text = "Pojazdy"; //--- ToDo: Localize;
+			x = 6.5 * GUI_GRID_W + GUI_GRID_X;
+			y = 20.5 * GUI_GRID_H + GUI_GRID_Y;
+			w = 6.5 * GUI_GRID_W;
+			h = 1.5 * GUI_GRID_H;
+		};
+		class button_applicant_reject: client_RscButtonMenu {
+			idc = 1207;
+			text = "Odrzuc"; //--- ToDo: Localize;
+			x = 33.5 * GUI_GRID_W + GUI_GRID_X;
+			y = 20.5 * GUI_GRID_H + GUI_GRID_Y;
+			w = 6.5 * GUI_GRID_W;
+			h = 1.5 * GUI_GRID_H;
+		};
+		class button_applicant_check: client_RscButtonMenu {
+			idc = 1208;
+			text = "Sprawdz"; //--- ToDo: Localize;
+			x = 27 * GUI_GRID_W + GUI_GRID_X;
+			y = 20.5 * GUI_GRID_H + GUI_GRID_Y;
+			w = 6.5 * GUI_GRID_W;
+			h = 1.5 * GUI_GRID_H;
+		};
+		class button_applicant_accept: client_RscButtonMenu {
+			idc = 1209;
+			text = "Przyjmij"; //--- ToDo: Localize;
+			x = 20.5 * GUI_GRID_W + GUI_GRID_X;
+			y = 20.5 * GUI_GRID_H + GUI_GRID_Y;
+			w = 6.5 * GUI_GRID_W;
+			h = 1.5 * GUI_GRID_H;
+		};
+		class button_close: client_RscButtonMenu {
+			idc = 1210;
+			text = "Zamknij"; //--- ToDo: Localize;
+			x = 27 * GUI_GRID_W + GUI_GRID_X;
+			y = 23 * GUI_GRID_H + GUI_GRID_Y;
+			w = 13 * GUI_GRID_W;
+			h = 2 * GUI_GRID_H;
+		};
+		class text_title: RscText {
+			idc = 1301;
+			text = "Przeglad firmy: <nazwa firmy>"; //--- ToDo: Localize;
+			x = 0 * GUI_GRID_W + GUI_GRID_X;
+			y = 0 * GUI_GRID_H + GUI_GRID_Y;
+			w = 40 * GUI_GRID_W;
+			h = 1 * GUI_GRID_H;
+			colorBackground[] = {0,0,0,0.5};
+		};
+		class text_title_vehicles: RscText {
+			idc = -1;
+			text = "Pojazdy"; //--- ToDo: Localize;
+			x = 0 * GUI_GRID_W + GUI_GRID_X;
+			y = 1.5 * GUI_GRID_H + GUI_GRID_Y;
+			w = 19.5 * GUI_GRID_W;
+			h = 1 * GUI_GRID_H;
+			colorBackground[] = {0,0,0,0.5};
+		};
+		class text_title_logs: RscText {
+			idc = -1;
+			text = "Logi"; //--- ToDo: Localize;
+			x = 0 * GUI_GRID_W + GUI_GRID_X;
+			y = 12 * GUI_GRID_H + GUI_GRID_Y;
+			w = 19.5 * GUI_GRID_W;
+			h = 1 * GUI_GRID_H;
+			colorBackground[] = {0,0,0,0.5};
+		};
+		class text_title_applicants: RscText {
+			idc = -1;
+			text = "Podania"; //--- ToDo: Localize;
+			x = 20.5 * GUI_GRID_W + GUI_GRID_X;
+			y = 12 * GUI_GRID_H + GUI_GRID_Y;
+			w = 19.5 * GUI_GRID_W;
+			h = 1 * GUI_GRID_H;
+			colorBackground[] = {0,0,0,0.5};
+		};
+		class text_title_employees: RscText {
+			idc = -1;
+			text = "Pracownicy"; //--- ToDo: Localize;
+			x = 20.5 * GUI_GRID_W + GUI_GRID_X;
+			y = 1.5 * GUI_GRID_H + GUI_GRID_Y;
+			w = 19.5 * GUI_GRID_W;
+			h = 1 * GUI_GRID_H;
+			colorBackground[] = {0,0,0,0.5};
+		};
 	};
 };
 class kruk_company_atm {
@@ -161,6 +406,158 @@ class kruk_company_atm {
 	enableSimulation = 1;
 	onLoad = "";
 	class controls {
-		
+		class edit_cash: Client_RscEdit {
+			idc = 1001;
+			text = "";
+			autocomplete = "";
+			x = 27 * GUI_GRID_W + GUI_GRID_X;
+			y = 6 * GUI_GRID_H + GUI_GRID_Y;
+			w = 13 * GUI_GRID_W;
+			h = 2 * GUI_GRID_H;
+			colorBackground[] = {0,0,0,0.5};
+		};
+		class listbox_players: client_RscListBox {
+			idc = 1101;
+			x = 13.5 * GUI_GRID_W + GUI_GRID_X;
+			y = 6 * GUI_GRID_H + GUI_GRID_Y;
+			w = 13 * GUI_GRID_W;
+			h = 4.5 * GUI_GRID_H;
+			colorBackground[] = {0,0,0,0.5};
+		};
+		class listbox_employees: client_RscListBox {
+			idc = 1102;
+			x = 13.5 * GUI_GRID_W + GUI_GRID_X;
+			y = 12 * GUI_GRID_H + GUI_GRID_Y;
+			w = 13 * GUI_GRID_W;
+			h = 4.5 * GUI_GRID_H;
+			colorBackground[] = {0,0,0,0.5};
+		};
+		class listbox_companies: client_RscListBox {
+			idc = 1103;
+			x = 13.5 * GUI_GRID_W + GUI_GRID_X;
+			y = 18 * GUI_GRID_H + GUI_GRID_Y;
+			w = 13 * GUI_GRID_W;
+			h = 4.5 * GUI_GRID_H;
+			colorBackground[] = {0,0,0,0.5};
+		};
+		class list_box_last_operations: client_RscListBox {
+			idc = 1104;
+			x = 0 * GUI_GRID_W + GUI_GRID_X;
+			y = 6 * GUI_GRID_H + GUI_GRID_Y;
+			w = 13 * GUI_GRID_W;
+			h = 16.5 * GUI_GRID_H;
+			colorBackground[] = {0,0,0,0.5};
+		};
+		class button_withdraw: client_RscButtonMenu {
+			idc = 1201;
+			text = "Wypłać"; //--- ToDo: Localize;
+			x = 27 * GUI_GRID_W + GUI_GRID_X;
+			y = 8 * GUI_GRID_H + GUI_GRID_Y;
+			w = 13 * GUI_GRID_W;
+			h = 1.5 * GUI_GRID_H;
+		};
+		class button_payin: client_RscButtonMenu {
+			idc = 1202;
+			text = "Wpłać"; //--- ToDo: Localize;
+			x = 27 * GUI_GRID_W + GUI_GRID_X;
+			y = 9.5 * GUI_GRID_H + GUI_GRID_Y;
+			w = 13 * GUI_GRID_W;
+			h = 1.5 * GUI_GRID_H;
+		};
+		class button_pay_player: client_RscButtonMenu {
+			idc = 1203;
+			text = "Przelej (osoba)"; //--- ToDo: Localize;
+			x = 27 * GUI_GRID_W + GUI_GRID_X;
+			y = 11 * GUI_GRID_H + GUI_GRID_Y;
+			w = 13 * GUI_GRID_W;
+			h = 1.5 * GUI_GRID_H;
+		};
+		class button_pay_employee: client_RscButtonMenu {
+			idc = 1204;
+			text = "Przelej (pracownik)"; //--- ToDo: Localize;
+			x = 27 * GUI_GRID_W + GUI_GRID_X;
+			y = 12.5 * GUI_GRID_H + GUI_GRID_Y;
+			w = 13 * GUI_GRID_W;
+			h = 1.5 * GUI_GRID_H;
+		};
+		class button_pay_company: client_RscButtonMenu {
+			idc = 1205;
+			text = "Przelej (firma)"; //--- ToDo: Localize;
+			x = 27 * GUI_GRID_W + GUI_GRID_X;
+			y = 14 * GUI_GRID_H + GUI_GRID_Y;
+			w = 13 * GUI_GRID_W;
+			h = 1.5 * GUI_GRID_H;
+		};
+		class button_close: client_RscButtonMenu {
+			idc = 1206;
+			text = "Zamknij"; //--- ToDo: Localize;
+			x = 27 * GUI_GRID_W + GUI_GRID_X;
+			y = 23 * GUI_GRID_H + GUI_GRID_Y;
+			w = 13 * GUI_GRID_W;
+			h = 2 * GUI_GRID_H;
+		};
+		class text_balance: RscText {
+			idc = 1301;
+			text = "Stan konta: $5000000"; //--- ToDo: Localize;
+			x = 0 * GUI_GRID_W + GUI_GRID_X;
+			y = 2 * GUI_GRID_H + GUI_GRID_Y;
+			w = 40 * GUI_GRID_W;
+			h = 2 * GUI_GRID_H;
+			colorBackground[] = {0,0,0,0.5};
+		};
+		class text_title: RscText {
+			idc = 1302;
+			text = "Nazwa firmy: "; //--- ToDo: Localize;
+			x = 0 * GUI_GRID_W + GUI_GRID_X;
+			y = 0 * GUI_GRID_H + GUI_GRID_Y;
+			w = 40 * GUI_GRID_W;
+			h = 1 * GUI_GRID_H;
+			colorBackground[] = {0,0,0,0.5};
+		};
+		class text_actions: RscText {
+			idc = -1;
+			text = "Akcje"; //--- ToDo: Localize;
+			x = 27 * GUI_GRID_W + GUI_GRID_X;
+			y = 5 * GUI_GRID_H + GUI_GRID_Y;
+			w = 13 * GUI_GRID_W;
+			h = 1 * GUI_GRID_H;
+			colorBackground[] = {0,0,0,0.5};
+		};
+		class text_last_operations: RscText {
+			idc = -1;
+			text = "Ostatnie operacje"; //--- ToDo: Localize;
+			x = 0 * GUI_GRID_W + GUI_GRID_X;
+			y = 5 * GUI_GRID_H + GUI_GRID_Y;
+			w = 13 * GUI_GRID_W;
+			h = 1 * GUI_GRID_H;
+			colorBackground[] = {0,0,0,0.5};
+		};
+		class text_players: RscText {
+			idc = -1;
+			text = "Osoby"; //--- ToDo: Localize;
+			x = 13.5 * GUI_GRID_W + GUI_GRID_X;
+			y = 5 * GUI_GRID_H + GUI_GRID_Y;
+			w = 13 * GUI_GRID_W;
+			h = 1 * GUI_GRID_H;
+			colorBackground[] = {0,0,0,0.5};
+		};
+		class text_companies: RscText {
+			idc = -1;
+			text = "Firmy"; //--- ToDo: Localize;
+			x = 13.5 * GUI_GRID_W + GUI_GRID_X;
+			y = 17 * GUI_GRID_H + GUI_GRID_Y;
+			w = 13 * GUI_GRID_W;
+			h = 1 * GUI_GRID_H;
+			colorBackground[] = {0,0,0,0.5};
+		};
+		class text_employees: RscText {
+			idc = -1;
+			text = "Pracownicy"; //--- ToDo: Localize;
+			x = 13.5 * GUI_GRID_W + GUI_GRID_X;
+			y = 11 * GUI_GRID_H + GUI_GRID_Y;
+			w = 13 * GUI_GRID_W;
+			h = 1 * GUI_GRID_H;
+			colorBackground[] = {0,0,0,0.5};
+		};
 	};
-};
+};   
