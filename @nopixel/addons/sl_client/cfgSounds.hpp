5f266943ea34462698b1b51626e12c2fb91032f2
JeffreyStanLakeside <rozwalaczewota@gmail.com>
2017-05-07 21:52:34 +0200

    kif_client to sl_client

---
+++ @nopixel/addons/sl_client/cfgSounds.hpp
@@ -0,0 +1,53 @@
+class CfgSounds
+{
+	sounds[] = {};
+	class bankAlarm
+	{
+		name="bankAlarm";
+		sound[]=
+		{"\sl_client\sounds\bankAlarm.ogg",1,1,100};
+		titles[] = {0,""};
+	};
+	class fireAlarm
+	{
+		name="fireAlarm";
+		sound[]=
+		{"\sl_client\sounds\fireAlarm.ogg",1,1,100};
+		titles[] = {0,""};
+	};
+	class shopAlarm
+	{
+		name="shopAlarm";
+		sound[]=
+		{"\sl_client\sounds\shopAlarm.ogg",1,1,100};
+		titles[] = {0,""};
+	};
+	class bandage
+	{
+		name="bandage";
+		sound[]=
+		{"\sl_client\sounds\bandage.ogg",1,1,100};
+		titles[] = {0,""};
+	};
+	class blindfold
+	{
+		name="blindfold";
+		sound[]=
+		{"\sl_client\sounds\blindfold.ogg",1,1,100};
+		titles[] = {0,""};
+	};
+	class speedbombexplode
+	{
+		name="speedbombexplode";
+		sound[]=
+		{"\sl_client\sounds\speedbombExplode.ogg",1,1,100};
+		titles[] = {0,""};
+	};
+	class bombhasbeenplanted
+	{
+		name="bombhasbeenplanted";
+		sound[]=
+		{"\sl_client\sounds\bombhasbeenplanted.ogg",1,1,100};
+		titles[] = {0,""};
+	};
+};
--- @nopixel/addons/sl_client/config.cpp
+++ @nopixel/addons/sl_client/config.cpp
@@ -5,7 +5,14 @@ class CfgPatches
  		units[] = {};
  		weapons[] = {};
  		requiredVersion = 0.1;
-		requiredAddons[]= {"A3_Data_F"};
+		requiredAddons[]= {
+			"A3_Data_F",
+			"plp_containers",
+			"ivory_data",
+			"ivory_isf",
+			"ivory_m3",
+             "np_ivory"
+		};
  	};
  };
 class cfgFunctions
@@ -22,14 +29,79 @@ class cfgFunctions
 		};
 	};
 };
+class CfgVehicleClasses
+{
+	class kif_client
+	{
+		displayName = "Stan Lakeside Buildings";
+	};
+};
+class CfgMagazines
+{
+	class InventoryItem_Base_F;
+	class BoxItem: InventoryItem_Base_F
+	{
+		type=620;
+		allowedSlots[]={901};
+		mass=16;
+	};
+	class Default;
+	class ItemCore: Default
+	{
+		type=131072;
+		model="\A3\weapons_F\ammo\mag_univ.p3d";
+		picture="\A3\Weapons_F\Data\clear_empty.paa";
+		muzzles[]={};
+		class Armory
+		{
+			disabled=1;
+		};
+	};
+	#include "\sl_client\cfgItems.hpp"
+};
 class CfgVehicles {
 	class All{};
 	class Static: All{};
 	class Building: Static{};
+	class ReammoBox;
+	class ThingX;
 	class B_Competitor_F;
+	class kif_chest_base: ThingX
+	{
+		animated=0;
+		scope=0;
+		scopeCurator=0;
+		editorCategory="EdCat_Supplies";
+		editorSubcategory="EdSubcat_Storage";
+		icon="iconCrate";
+		model="\A3\Weapons_f\dummyweapon.p3d";
+		accuracy=0.2;
+		typicalCargo[]={};
+		vehicleClass="plp_containers";
+		destrType="DestructNo";
+		class DestructionEffects
+		{
+		};
+		maximumLoad=500;
+		transportMaxWeapons=5;
+		transportMaxMagazines=100;
+		transportMaxBackpacks=2;
+		transportAmmo=0;
+		transportRepair=0;
+		transportFuel=0;
+		supplyRadius=1.4;
+		cost=0;
+		armor=200;
+		mapSize=2;
+		waterLinearDampingCoefY=1;
+		waterAngularDampingCoef=0.1;
+	};
+        #include "\sl_client\cfgHouseStorages.hpp"
 	#include "\sl_client\cfgPDUniformsV.hpp"
 	#include "\sl_client\cfgBuildings.hpp"
 };
+#include "\sl_client\cfgSounds.hpp"
+#include "\sl_client\cfgMainMenu.hpp"
 class cfgWeapons {
 	class ItemCore;
 	class Uniform_Base;
@@ -42,4 +114,6 @@ class cfgWeapons {
 	class HeadgearItem;
 	#include "\sl_client\cfgPDVests.hpp"
 	#include "\sl_client\cfgPDUniformsW.hpp"
+	#include "\sl_client\cfgPDCaps.hpp"
+	#include "\sl_client\cfgItemsWeapons.hpp"
 };
