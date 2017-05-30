0a1cd3eb94405ef1952e3c197fa384b1404dd924
Jeffrey <rozwalaczewota@gmail.com>
2017-05-30 18:16:22 +0200

    syreny

--- @nopixel/addons/sl_vehicles_red/data/sirenArray.hpp
+++ @nopixel/addons/sl_vehicles_red/data/sirenArray.hpp
@@ -1,5 +1,2 @@
-SirenArray[] = {
-    "DD_Wail_P1",
-	"DD_Yelp_P1",
-    "DD_Phaser_P1"
-};
+tf_hasLRradio = 1;
+SirenArray[] = {"DD_Manual_Rumbler_P1",	"DD_Wail_Rumbler_P1",	"DD_Yelp_Rumbler_P1",	"DD_Phaser_Rumbler_P1"};
